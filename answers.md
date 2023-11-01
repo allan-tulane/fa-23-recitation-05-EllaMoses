58791dfaf854dd389702f56f56966938ac6312f0
EllaMoses <emoses@tulane.edu>
2023-11-01 00:09:17 -0500

    Code and Answers

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 6
 ## Answers
 
-**Name:**_________________________
+**Name:** Ella Moses
 
 
 Place all written answers from `recitation-06.md` here for easier grading.
@@ -10,17 +10,19 @@ Place all written answers from `recitation-06.md` here for easier grading.
 
 - **d.**
 
-File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
-----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
+| filename     |   fixed-length-cost |   huffman-cost |   ratio |
+|--------------|---------------------|----------------|---------|
+| alice29.txt  |             1039367 |         676374 |   0.651 |
+| asyoulik.txt |              876253 |         606448 |   0.692 |
+| f1.txt       |                1340 |            826 |   0.616 |
+| fields.c     |               78050 |          56206 |   0.720 |
+| grammar.lsp  |               26047 |          17356 |   0.666 |
+
+The ratio of huffman-cost to fixed-length-cost seems to be between 0.6 and 0.7. This means that the huffman cost is constistantly lower than the fixed cost and the ratio is approximately the same regardless of file size. 
 
 
 
 
 - **e.**
-
+If we use huffman encoding on a file and every character has the same frequency f, then the length would be equal to f + 2f + 3f + ... + (n-1)f + nf + nf, where n equals the number of characters. Regardless of the document, if each character has the same frequency nf + f * ((n+1)/2)
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math, queue
 from collections import Counter
+import tabulate 
 
 class TreeNode(object):
     # we assume data is a tuple (frequency, character)
@@ -29,33 +30,87 @@ def make_huffman_tree(f):
     # the leaves of the final tree
     for c in f.keys():
         p.put(TreeNode(None,None,(f[c], c)))
-
     # greedily remove the two nodes x and y with lowest frequency,
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
+        min1 = p.get()
+        min2= p.get()
+        sum = min1.data[0] + min2.data[0]
+        if min1.data[1] == '':
+            z = TreeNode(min2, min1, (sum, ""))
+        else:
+            z = TreeNode(min1, min2, (sum, ""))
+        p.put(z)
+        #print(z.data)
+
         
     # return root of the tree
     return p.get()
 
+
 # perform a traversal on the prefix code tree to collect all encodings
-def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+def get_code(node, prefix="", code={}):  
+    if node.left != None:
+        get_code(node.left, prefix + '0', code)
+    if node.right != None:
+        get_code(node.right, prefix + '1', code)
+    else:
+        code[node.data[1]] = prefix
+    return code
+
+
+        
+        
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
+    #for fixed length coding, we need to represent each character with the same number of bits
+    num_characters = len(f) #number of different characters
+    num_bits = math.ceil(math.log(num_characters, 2)) #number of bits needed to represent the number of characters
+    total = (sum(f.values())) #number of characters
+    return total*num_bits #number of characters * number of bits per character = how many bits are needed to ecode the text
 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
-    # TODO
-    pass
+    sum = 0
+    for key in f:
+        count = f[key]
+        code = C[key]
+        bits = count * len(code)
+        sum += bits
+                
+    return sum
 
+'''
 f = get_frequencies('f1.txt')
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
 T = make_huffman_tree(f)
 C = get_code(T)
 print("Huffman cost:  %d" % huffman_cost(C, f))
+'''
+
+def compare_methods(files):
+    result = []
+    for file in files:
+        f = get_frequencies(file)
+        print(f)
+        print(len(f))
+        fixed_cost = fixed_length_cost(f)
+        T = make_huffman_tree(f)
+        C = get_code(T)
+        #print(C)
+        huff_cost = huffman_cost(C, f)
+        ratio = huff_cost/fixed_cost
+        result.append([file, fixed_cost, huff_cost, ratio])
+    return result
+
+def print_results(results):
+    print(tabulate.tabulate(results,
+                            headers=['filename', 'fixed-length-cost', 'huffman-cost', 'ratio'],
+                            floatfmt=".3f",
+                            tablefmt="github"))
+ 
+files = ['alice29.txt', 'asyoulik.txt', 'f1.txt', 'fields.c', 'grammar.lsp'] 
+files2 = ['f1.txt'] 
+print_results(compare_methods(files))
